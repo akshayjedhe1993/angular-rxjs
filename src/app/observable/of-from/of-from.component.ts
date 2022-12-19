9e5c0760eb650b55d7deeb2be2d7c8a0ed70870e
akshayjedhe1993 <120628350+akshayjedhe1993@users.noreply.github.com>
2022-12-19 11:28:44 +0530

    2 files modified

--- src/app/observable/of-from/of-from.component.ts
+++ src/app/observable/of-from/of-from.component.ts
@@ -1,15 +1,55 @@
 import { Component, OnInit } from '@angular/core';
+import { from, of } from 'rxjs';
+import { DesignUtilityService } from '../../appServices/design-utility.service';
 
 @Component({
   selector: 'app-of-from',
   templateUrl: './of-from.component.html',
-  styleUrls: ['./of-from.component.css']
+  styleUrls: ['./of-from.component.css'],
 })
 export class OfFromComponent implements OnInit {
-
-  constructor() { }
+  objMsg;
+  constructor(private _designUtility: DesignUtilityService) {}
 
   ngOnInit() {
-  }
+    //  OF
+    const obj1 = of('Akshay', 'Gajanan', 'Jedhe');
+    obj1.subscribe((res) => {
+      this._designUtility.print(res, 'elContainer1');
+    });
 
+    const obj2 = of({
+      firstName: 'Akshay',
+      middleName: 'Gajanan',
+      lastName: 'Jedhe',
+    });
+    obj2.subscribe((res) => {
+      this.objMsg = res;
+    });
+
+    //  FROM - Array
+    let arr = ['Akshay', 'Gajanan', 'Jedhe'];
+    const obj3 = from(arr);
+    obj3.subscribe((res) => {
+      this._designUtility.print(res, 'elContainer3');
+    });
+
+    //  FROM - Promise
+    const promise = new Promise((resolve) => {
+      setTimeout(() => {
+        resolve('Promise Resolved');
+      }, 3000);
+    });
+
+    const obj4 = from(promise);
+    obj4.subscribe((res) => {
+      this._designUtility.print(res, 'elContainer4');
+    });
+
+    //  FROM - String
+    const obj5 = from('Welcome all');
+    obj5.subscribe((res) => {
+      this._designUtility.print(res, 'elContainer5');
+    });
+  }
 }
