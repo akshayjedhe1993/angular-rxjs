e8ba1abadc64f543b8ba4309f676d25e095b7af3
akshayjedhe1993 <120628350+akshayjedhe1993@users.noreply.github.com>
2022-12-19 19:40:13 +0530

    2 files modified

--- src/app/observable/map/map.component.ts
+++ src/app/observable/map/map.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
-  styleUrls: ['./map.component.css']
+  styleUrls: ['./map.component.css'],
 })
 export class MapComponent implements OnInit {
-
-  constructor() { }
+  sub1: Subscription;
+  constructor() {}
 
   ngOnInit() {
-  }
+    //  Ex.1
+    const broadcastVideo = interval(1000);
 
+    this.sub1 = broadcastVideo
+      .pipe(map((data) => 'Video ' + data))
+      .subscribe((res) => {
+        console.log(res);
+      });
+
+    setTimeout(() => {
+      this.sub1.unsubscribe();
+    }, 10000);
+  }
 }
