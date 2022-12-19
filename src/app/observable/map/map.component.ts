fad0f804412061890a8b2a10ccee73dc057fd8f4
akshayjedhe1993 <120628350+akshayjedhe1993@users.noreply.github.com>
2022-12-19 20:23:56 +0530

    2 files modified

--- src/app/observable/map/map.component.ts
+++ src/app/observable/map/map.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { interval, Subscription } from 'rxjs';
+import { from, interval, Subscription } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { DesignUtilityService } from '../../appServices/design-utility.service';
 
 @Component({
   selector: 'app-map',
@@ -9,20 +10,48 @@ import { map } from 'rxjs/operators';
 })
 export class MapComponent implements OnInit {
   sub1: Subscription;
-  constructor() {}
+  sub2: Subscription;
+  msg1;
+  msg2;
+  constructor(private _designUtility: DesignUtilityService) {}
 
   ngOnInit() {
-    //  Ex.1
     const broadcastVideo = interval(1000);
 
+    //  Ex.1 'Video'+ Data
     this.sub1 = broadcastVideo
       .pipe(map((data) => 'Video ' + data))
       .subscribe((res) => {
-        console.log(res);
+        // console.log(res);
+        this.msg1 = res;
       });
 
     setTimeout(() => {
       this.sub1.unsubscribe();
     }, 10000);
+
+    //  Ex.2
+    this.sub2 = broadcastVideo
+      .pipe(map((data) => data + 10))
+      .subscribe((res) => {
+        // console.log(res);
+        this.msg2 = res;
+      });
+
+    setTimeout(() => {
+      this.sub2.unsubscribe();
+    }, 10000);
+
+    //  Ex.3
+    const members = from([
+      { id: 1, name: 'Akshay' },
+      { id: 2, name: 'Gajanan' },
+      { id: 3, name: 'Jedhe' },
+    ]);
+
+    members.pipe(map((data) => data.name)).subscribe((res) => {
+      console.log(res);
+      this._designUtility.print(res, 'elContainer');
+    });
   }
 }
