a951df970b6bebd0500ae2f3db6487e6aaada5e0
akshayjedhe1993 <120628350+akshayjedhe1993@users.noreply.github.com>
2022-12-19 13:39:45 +0530

    4 files modified

--- src/app/observable/custom/custom.component.ts
+++ src/app/observable/custom/custom.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-custom',
   templateUrl: './custom.component.html',
-  styleUrls: ['./custom.component.css']
+  styleUrls: ['./custom.component.css'],
 })
 export class CustomComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-  }
+    //   Ex.1 (Manual)
+    const cusObs1 = Observable.create((observer) => {
+      setTimeout(() => {
+        observer.next('Data Emit 1');
+      }, 1000);
+      setTimeout(() => {
+        observer.next('Data Emit 2');
+      }, 1000);
+      setTimeout(() => {
+        observer.next('Data Emit 3');
+      }, 1000);
+    });
 
+    cusObs1.subscribe((res) => {
+      console.log(res);
+    });
+  }
 }
