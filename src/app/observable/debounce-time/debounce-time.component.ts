2485dff1d4bdd67179a356c538ec876b0d28a89d
akshayjedhe1993 <120628350+akshayjedhe1993@users.noreply.github.com>
2022-12-20 16:01:10 +0530

    2 files modified

--- src/app/observable/debounce-time/debounce-time.component.ts
+++ src/app/observable/debounce-time/debounce-time.component.ts
@@ -6,7 +6,7 @@ import {
   ViewChild,
 } from '@angular/core';
 import { fromEvent } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'app-debounce-time',
@@ -15,20 +15,41 @@ import { map } from 'rxjs/operators';
 })
 export class DebounceTimeComponent implements AfterViewInit {
   @ViewChild('myInput') myInput: ElementRef;
+  @ViewChild('myInput2') myInput2: ElementRef;
   reqData = null;
+  reqData2 = null;
 
   constructor() {}
 
   ngAfterViewInit() {
+    //  Ex.1 DebounceTime
     const searchTerm = fromEvent<any>(this.myInput.nativeElement, 'keyup').pipe(
-      map(event => event.target.value)
+      map((event) => event.target.value),
+      debounceTime(1000)
     );
     searchTerm.subscribe((res) => {
       this.reqData = res;
       console.log(res);
       setTimeout(() => {
         this.reqData = null;
-      },1000);
+      }, 2000);
+    });
+
+    //  Ex.2 DistinctUntilChanged
+    const searchTerm2 = fromEvent<any>(
+      this.myInput2.nativeElement,
+      'keyup'
+    ).pipe(
+      map((event) => event.target.value),
+      debounceTime(1000),
+      distinctUntilChanged()
+    );
+    searchTerm2.subscribe((res) => {
+      this.reqData2 = res;
+      console.log(res);
+      setTimeout(() => {
+        this.reqData2 = null;
+      }, 2000);
     });
   }
 }
