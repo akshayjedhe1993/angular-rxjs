5ae3d21f739fa855fc95b7101186926da282f514
akshayjedhe1993 <120628350+akshayjedhe1993@users.noreply.github.com>
2022-12-20 15:51:03 +0530

    2 files modified

--- src/app/observable/debounce-time/debounce-time.component.ts
+++ src/app/observable/debounce-time/debounce-time.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { fromEvent } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-debounce-time',
   templateUrl: './debounce-time.component.html',
-  styleUrls: ['./debounce-time.component.css']
+  styleUrls: ['./debounce-time.component.css'],
 })
-export class DebounceTimeComponent implements OnInit {
+export class DebounceTimeComponent implements AfterViewInit {
+  @ViewChild('myInput') myInput: ElementRef;
+  reqData = null;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngAfterViewInit() {
+    const searchTerm = fromEvent<any>(this.myInput.nativeElement, 'keyup').pipe(
+      map(event => event.target.value)
+    );
+    searchTerm.subscribe((res) => {
+      this.reqData = res;
+      console.log(res);
+      setTimeout(() => {
+        this.reqData = null;
+      },1000);
+    });
   }
-
 }
