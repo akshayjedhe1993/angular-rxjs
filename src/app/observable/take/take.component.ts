8b5fea0623a7a6238f4663710b286bb423eabbe4
akshayjedhe1993 <120628350+akshayjedhe1993@users.noreply.github.com>
2022-12-20 14:07:01 +0530

    2 files modified

--- src/app/observable/take/take.component.ts
+++ src/app/observable/take/take.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { interval } from 'rxjs';
-import { take } from 'rxjs/operators';
+import { from, fromEvent, interval, timer } from 'rxjs';
+import { map, take, takeLast, takeUntil } from 'rxjs/operators';
+import { DesignUtilityService } from '../../appServices/design-utility.service';
 
 @Component({
   selector: 'app-take',
@@ -8,13 +9,38 @@ import { take } from 'rxjs/operators';
   styleUrls: ['./take.component.css'],
 })
 export class TakeComponent implements OnInit {
-  constructor() {}
+  constructor(private _du: DesignUtilityService) {}
+
+  randomNames = ['Akshay', 'Gajanan', 'Jedhe', 'Angular', 'NC', 'FE'];
 
   ngOnInit() {
-    const source = interval(1000).pipe(take(4));
+    const nameSource = from(this.randomNames);
 
-    source.subscribe((res) => {
-      console.log(res);
+    //  EX.1 Take
+    nameSource.pipe(take(3)).subscribe((res) => {
+      // console.log(res);
+      this._du.print(res, 'elContainer1');
     });
+
+    //  EX.2 TakeLast
+    nameSource.pipe(takeLast(3)).subscribe((res) => {
+      // console.log(res);
+      this._du.print(res, 'elContainer2');
+    });
+
+    //  EX.3 TakeUntil
+    const source = interval(1000);
+    let condition1 = timer(5000);
+    let condition2 = fromEvent(document, 'click');
+
+    source
+      .pipe(
+        map((data) => 'Number ' + data),
+        takeUntil(condition2)
+      )
+      .subscribe((res) => {
+        console.log(res);
+        this._du.print(res, 'elContainer3');
+      });
   }
 }
