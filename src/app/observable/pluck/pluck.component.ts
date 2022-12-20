ab5aa50cac968c3e7453bb4f7fc89bbe1a736403
akshayjedhe1993 <120628350+akshayjedhe1993@users.noreply.github.com>
2022-12-20 10:51:37 +0530

    5 files modified, 3 added

--- src/app/observable/pluck/pluck.component.ts
+++ src/app/observable/pluck/pluck.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { from } from 'rxjs';
+import { map, pluck, toArray } from 'rxjs/operators';
 
 @Component({
   selector: 'app-pluck',
@@ -8,5 +10,47 @@ import { Component, OnInit } from '@angular/core';
 export class PluckComponent implements OnInit {
   constructor() {}
 
-  ngOnInit() {}
+  data;
+  data2;
+  users = [
+    {
+      id: 1,
+      name: 'Akshay',
+      job: { title: 'Frontend Developer', EXP: '10 years' },
+    },
+    {
+      id: 2,
+      name: 'Gajanan',
+      job: { title: 'Backend Developer', EXP: '9 years' },
+    },
+    {
+      id: 3,
+      name: 'Jedhe',
+      job: { title: 'Designer', EXP: '11 years' },
+    },
+  ];
+
+  ngOnInit() {
+    //  Ex.1 By Property
+    from(this.users)
+      .pipe(
+        // map((data) => data.name),
+        pluck('name'),
+        toArray()
+      )
+      .subscribe((res) => {
+        this.data = res;
+      });
+
+    //  Ex.2
+    from(this.users)
+      .pipe(
+        // map((data) => data.name),
+        pluck('job', 'title'),
+        toArray()
+      )
+      .subscribe((res) => {
+        this.data2 = res;
+      });
+  }
 }
