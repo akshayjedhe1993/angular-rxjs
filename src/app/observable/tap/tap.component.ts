56fa49bf69821d21a1431ec7fd22fe86b7ab8045
akshayjedhe1993 <120628350+akshayjedhe1993@users.noreply.github.com>
2022-12-20 11:17:17 +0530

    2 files modified

--- src/app/observable/tap/tap.component.ts
+++ src/app/observable/tap/tap.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-tap',
   templateUrl: './tap.component.html',
-  styleUrls: ['./tap.component.css']
+  styleUrls: ['./tap.component.css'],
 })
 export class TapComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    //  Ex.1
+    const arr = ['Akshay', 'Gajanan', 'Jedhe'];
+    const source = interval(1500);
+    source.pipe(map((data) => arr[data])).subscribe((res) => {
+      console.log(res);
+    });
   }
-
 }
