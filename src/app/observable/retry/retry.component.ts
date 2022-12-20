8889aea0c9d5a72ac9c944c1e2fdde521f37f83c
akshayjedhe1993 <120628350+akshayjedhe1993@users.noreply.github.com>
2022-12-20 15:22:49 +0530

    2 files modified

--- src/app/observable/retry/retry.component.ts
+++ src/app/observable/retry/retry.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { retry, retryWhen, delay, scan } from 'rxjs/operators';
 
 @Component({
   selector: 'app-retry',
@@ -8,16 +9,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RetryComponent implements OnInit {
   constructor(private http: HttpClient) {}
+  data;
+  fetching: boolean = false;
+  status = 'No data';
 
-  ngOnInit() {
-    this.fetchDetails();
-  }
+  ngOnInit() {}
 
   fetchDetails() {
+    this.fetching = true;
+    this.status = 'Fetching Data';
     this.http
-      .get('https://global-1bb0f.firebaseio.com/user.json')
-      .subscribe((res) => {
-        console.log(res);
-      });
+      .get('https://jsonplaceholder.typicode.com/posts/1')
+      .pipe(
+        // retry(5)
+        retryWhen((err) =>
+          err.pipe(
+            delay(3000),
+            scan((retryCount) => {
+              if (retryCount >= 5) {
+                throw err;
+              } else {
+                retryCount += 1;
+                console.log('retryCount', retryCount);
+                this.status = 'Retrying attempts #' + retryCount;
+                return retryCount;
+              }
+            }, 0)
+          )
+        )
+      )
+      .subscribe(
+        (res) => {
+          console.log(res);
+          this.data = res;
+          this.status = 'Data fetched';
+          this.fetching = false;
+        },
+        (error) => {
+          console.log(error);
+          this.status = 'Problem fetching data';
+          this.fetching = false;
+        }
+      );
   }
 }
