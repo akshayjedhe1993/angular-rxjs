28d8f640ced739191adfab6a0f2f68fef9af66fe
akshayjedhe1993 <120628350+akshayjedhe1993@users.noreply.github.com>
2022-12-20 16:28:26 +0530

    3 files modified

--- src/app/includes/header/header.component.ts
+++ src/app/includes/header/header.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { DesignUtilityService } from '../../appServices/design-utility.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  navOpen: boolean = false;
+  exclusive: boolean = false;
 
-  constructor() { }
+  constructor(private _du: DesignUtilityService) {}
 
   ngOnInit() {
+    this._du.exclusive.subscribe(res => {
+      // this.exclusive = res;
+    })
   }
 
+  onNavToggle() {
+    this.navOpen = !this.navOpen;
+  }
 }
