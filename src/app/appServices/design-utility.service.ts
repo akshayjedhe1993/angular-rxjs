372c5b6774fcabb2ad7a36279743e1a391a5b7e8
akshayjedhe1993 <120628350+akshayjedhe1993@users.noreply.github.com>
2022-12-21 12:46:52 +0530

    4 files modified

--- src/app/appServices/design-utility.service.ts
+++ src/app/appServices/design-utility.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Subject } from 'rxjs';
+import { BehaviorSubject, ReplaySubject, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -9,6 +9,8 @@ export class DesignUtilityService {
   // in Subject we can not set initial value
   exclusive = new Subject<boolean>();
   userName = new BehaviorSubject<string>('Akshay');
+
+  videoEmit = new ReplaySubject<string>(3, 5000);
 
   constructor() {}
 
--- src/app/observable/replay-subject/replay-subject.component.ts
+++ src/app/observable/replay-subject/replay-subject.component.ts
@@ -1,4 +1,7 @@
+import { IfStmt } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
+import { DesignUtilityService } from '../../appServices/design-utility.service';
 
 @Component({
   selector: 'app-replay-subject',
@@ -6,7 +9,62 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./replay-subject.component.css'],
 })
 export class ReplaySubjectComponent implements OnInit {
-  constructor() {}
+  user1List = ['Angular 1', 'Angular 2'];
+  user2List = [];
+  user3List = [];
 
-  ngOnInit() {}
+  subscribeMode2: boolean = false;
+  subscribeMode3: boolean = false;
+
+  subscription2: Subscription;
+  subscription3: Subscription;
+
+  methodInterval: boolean = false;
+  intSubscription: Subscription;
+
+  constructor(private _designUtility: DesignUtilityService) {}
+
+  ngOnInit() {
+    this._designUtility.videoEmit.subscribe((res) => {
+      this.user1List.push(res);
+    });
+  }
+
+  onVideoAdd(video) {
+    this._designUtility.videoEmit.next(video.value);
+  }
+
+  user2Subscribe() {
+    if (this.subscription2) {
+      this.subscription2.unsubscribe();
+    } else {
+      this.subscription2 = this._designUtility.videoEmit.subscribe((res) => {
+        this.user2List.push(res);
+      });
+    }
+    this.subscribeMode2 = !this.subscribeMode2;
+  }
+
+  user3Subscribe() {
+    if (this.subscription3) {
+      this.subscription3.unsubscribe();
+    } else {
+      this.subscription3 = this._designUtility.videoEmit.subscribe((res) => {
+        this.user3List.push(res);
+      });
+    }
+    this.subscribeMode3 = !this.subscribeMode3;
+  }
+
+  toggleMethod() {
+    const broadcastVideo = interval(1000);
+    if (!this.methodInterval) {
+      this.intSubscription = broadcastVideo.subscribe((res) => {
+        this._designUtility.videoEmit.next('Video ' + res);
+      });
+    } else {
+      this.intSubscription.unsubscribe();
+    }
+    this.methodInterval = !this.methodInterval;
+  }
 }
