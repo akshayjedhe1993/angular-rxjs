e8342501cec9fedeebbff4dc4bed24d0b9eedfe5
akshayjedhe1993 <120628350+akshayjedhe1993@users.noreply.github.com>
2022-12-23 12:11:16 +0530

    2 files modified

--- src/app/observable/concat-map/concat-map.component.ts
+++ src/app/observable/concat-map/concat-map.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { from, of, delay } from 'rxjs';
+import { concatAll, concatMap, map, mergeMap } from 'rxjs/operators';
+import { DesignUtilityService } from '../../appServices/design-utility.service';
 
 @Component({
   selector: 'app-concat-map',
   templateUrl: './concat-map.component.html',
-  styleUrls: ['./concat-map.component.css']
+  styleUrls: ['./concat-map.component.css'],
 })
 export class ConcatMapComponent implements OnInit {
-
-  constructor() { }
+  constructor(private _du: DesignUtilityService) {}
 
   ngOnInit() {
+    //  map + concatAll = concatMap
+
+    const source = from(['Tech', 'Comedy', 'News']);
+
+    //  Ex.1 Map
+    source.pipe(map((res) => this.getData(res))).subscribe((res) => {
+      // res.subscribe((res2) => {
+      //   console.log(res2);
+      this._du.print(res, 'elContainer1');
+      // });
+    });
+
+    //  Ex.2 mergeMap
+    source.pipe(mergeMap((res) => this.getData(res))).subscribe((res) => {
+      // console.log(res);
+      this._du.print(res, 'elContainer2');
+    });
+
+    //  Ex.3 concatMap
+    source.pipe(concatMap((res) => this.getData(res))).subscribe((res) => {
+      // console.log(res);
+      this._du.print(res, 'elContainer3');
+    });
   }
 
+  getData(data) {
+    return of(data + ' Video Uploaded').pipe(delay(2000));
+  }
 }
