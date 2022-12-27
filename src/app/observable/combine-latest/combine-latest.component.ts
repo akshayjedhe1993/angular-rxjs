3edbeb89dc6aac4b91e487f17e78a0f537b9907e
akshayjedhe1993 <120628350+akshayjedhe1993@users.noreply.github.com>
2022-12-27 11:49:05 +0530

    4 files modified

--- src/app/observable/combine-latest/combine-latest.component.ts
+++ src/app/observable/combine-latest/combine-latest.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { combineLatest, fromEvent } from 'rxjs';
+import { map, pluck, withLatestFrom } from 'rxjs/operators';
 
 @Component({
   selector: 'app-combine-latest',
   templateUrl: './combine-latest.component.html',
-  styleUrls: ['./combine-latest.component.css']
+  styleUrls: ['./combine-latest.component.css'],
 })
-export class CombineLatestComponent implements OnInit {
+export class CombineLatestComponent implements AfterViewInit {
+  nameSource = ['Akshay', 'Gajanan', 'Jedhe', 'Pooja', 'Sambhaji', 'Randive'];
+  colorSource = ['red', 'green', 'blue', 'yellow', 'violet', 'purple'];
 
-  constructor() { }
+  @ViewChild('name') name: ElementRef;
+  @ViewChild('color') color: ElementRef;
 
-  ngOnInit() {
+  constructor() {}
+
+  ngAfterViewInit() {
+    const nameObs = fromEvent<any>(this.name.nativeElement, 'change').pipe(
+      map((event) => event.target.value)
+    );
+
+    const colorObs = fromEvent<any>(this.color.nativeElement, 'change').pipe(
+      pluck('target', 'value')
+    );
+
+    //  Ex.1 combineLatest
+    combineLatest(nameObs, colorObs).subscribe(([name, color]) => {
+      // console.log(name, color);
+      this.createBox(name, color, 'elContainer1');
+    });
+
+    //  Ex.1 WithLatestFrom
+    //  master -> nameObs
+    //  slave -> colorObs
+    nameObs.pipe(withLatestFrom(colorObs)).subscribe(([name, color]) => {
+      // console.log(name, color);
+      this.createBox(name, color, 'elContainer2');
+    });
   }
 
+  createBox(name, color, containerId) {
+    let el = document.createElement('div');
+    el.innerText = name;
+    el.setAttribute('class', color);
+    document.getElementById(containerId).appendChild(el);
+  }
 }
